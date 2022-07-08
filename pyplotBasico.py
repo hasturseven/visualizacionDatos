4b798b34510441026064b58534b039ba32faade1
yjleon <yjleona7@gmail.com>
2022-07-08 16:37:40 -0500

    hasta plot orientado a objetos

--- pyplotBasico.py
+++ pyplotBasico.py
@@ -9,7 +9,20 @@ y=x**2
 #plt.plot para darle los valores y la construta
 #plt.plot(x,y)
 
-#AHORA VAMOS A MODIFICAR LA GRAFICA PARA PRESENTARLA COMO YO QUIERA arriba me la prsentara de una forma basica aca la presentare como yo quiera
-plt.plot(x,y,'y','rx')
+#AHORA VAMOS A MODIFICAR LA GRAFICA PARA PRESENTARLA COMO YO QUIERA arriba me la prsentara de una forma basica aca la presentare como yo quiera el 'rx'  la primera linea es que me presente la linea en rojo y el x es que sea en x y el o es en circulos y asi si le agregamos una linea indica continuidad - , y con : indicara consecucion de puntos etc puedo dejarla sola osea los valores o solo el y quye es el color o yD que sran diamantes etc...
+plt.plot(x,y,'yx-')
 #y el show para ver la grafica
+plt.show()
+"""AHORA GRAFICARE UN HISTO GRAMA QUE ES HISTORIA DE VALORES DE Y"""
+plt.hist(y)
+plt.show()
+
+"""graficandolo de otra manera en este caso una grafica de pie"""
+plt.pie(y)
+plt.show()
+"""AHORA CRANDO CORRELACION ENTRE LAS VARIABLES X AND Y"""
+plt.scatter(x,y)
+plt.show()
+"""DISTRIBUCION DE LOS DATOS ME GENERA UNA GRAFICA DE COMO SE ESTAN DISTRIBUYENDO LOS DATOS"""
+plt.boxplot(x)
 plt.show()
